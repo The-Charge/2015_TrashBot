cd4dcaf5439c56d3f6cbc3d1aaeaa79af18cd8ed
Nathan Collins <nc23626@gmail.com>
2015-02-12 20:45:36 -0500

    added tashbin auton comand

---
+++ src/Commands/tashbin_auton.h
@@ -0,0 +1,13 @@
+#ifndef tashbin_auton_H
+#define tashbin_auton_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class tashbin_auton: public CommandGroup
+{
+public:
+	tashbin_auton();
+};
+
+#endif
