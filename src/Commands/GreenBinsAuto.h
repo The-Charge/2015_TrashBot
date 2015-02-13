5e2d7dbc549e842a6987d0152ed0f528b31cba8b
Logan Cummings <logancummings4@gmail.com>
2015-02-12 19:05:37 -0500

    Merged and created command

---
+++ src/Commands/GreenBinsAuto.h
@@ -0,0 +1,13 @@
+#ifndef GreenBinsAuto_H
+#define GreenBinsAuto_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class GreenBinsAuto: public CommandGroup
+{
+public:
+	GreenBinsAuto();
+};
+
+#endif
