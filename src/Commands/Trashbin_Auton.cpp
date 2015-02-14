c4da04dff71dc8da56668b87450371f3e15683ea
Nathan Collins <nc23626@gmail.com>
2015-02-14 15:44:20 -0500

    added timeout to test auton

--- src/Commands/Trashbin_Auton.cpp
+++ src/Commands/Trashbin_Auton.cpp
@@ -8,26 +8,35 @@ Trashbin_Auton::Trashbin_Auton()
 
 
 	//time outs & flip
+
 	AddSequential(new DriveXFeet(-2.15833));
 	AddSequential(new WinchOut());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(1));
 	AddSequential(new WinchIn());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(-1));
 	AddSequential(new DriveXFeet(-5.725));
 	AddSequential(new WinchOut());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(1));
 	AddSequential(new WinchIn());
 	AddSequential(new StrafeXFeet(-1));
+	SetTimeout(10);//test
 
 	AddSequential(new DriveXFeet(-8.1083));
 	AddSequential(new WinchOut());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(1));
 	AddSequential(new WinchIn());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(-1));
 	AddSequential(new DriveXFeet(-5.725));
 	AddSequential(new WinchOut());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(1));
 	AddSequential(new WinchIn());
+	SetTimeout(10);//test
 	AddSequential(new StrafeXFeet(-1));
 
 }
