51b5b8ef748859591bfdcf7baa1e16537ab18090
Cole Schaar <schaarCA@gmail.com>
2015-02-15 18:08:11 -0500

    DO NOT USE THIS BRANCH, deleted many of the commands and subsystems for the sole purpose of finding the errors in the winch subsystem

--- src/Util/RobotStrafePIDOutput.cpp
+++ src/Util/RobotStrafePIDOutput.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "RobotStrafePIDOutput.h"
-#include "../Subsystems/DriveTrain.h"
+
 
 RobotStrafePIDOutput::RobotStrafePIDOutput(RobotDrive * drive) {
 	// TODO Auto-generated constructor stub
@@ -14,10 +14,9 @@ RobotStrafePIDOutput::RobotStrafePIDOutput(RobotDrive * drive) {
 }
 
 void RobotStrafePIDOutput::PIDWrite(double x) {
-	float rate = Robot::driveTrain->GetGyroRate();
+
 	//float correction = SmartDashboard::GetNumber(GYRO_CORRECTION_DASHBOARD_KEY,
 		//	CORRECTION_DEFAULT);
-	_drive->MecanumDrive_Cartesian(x, 0, 0, rate);
 	/*
 	 Robot::driveTrain -> rightRearPIDController -> SetTolerance(output);
 	 Robot::driveTrain -> leftRearPIDController -> SetTolerance(output);
