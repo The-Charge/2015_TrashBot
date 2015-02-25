73218a41be342eb31509b5a88ed7741e910a5ba4
Sparky-1 <richardw@hesonline.com>
2015-02-24 20:16:36 -0500

    Added the 2 tote stack and 3 tote stack to the sendable chooser for autonomous

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -82,10 +82,12 @@ void Robot::RobotInit() {
 	pushAuton = new std::string ("p");
 	pushGreenBins = new std::string("b");
 	ToteStack3Auton = new std::string("3t");
-	ToteStack3Auton = new std::string ("2t");
+	ToteStack2Auton = new std::string ("2t");
 	sc -> AddDefault("Drive1Foot", drive1Auton);
 	sc -> AddObject("PushOtherRobot", pushAuton);
 	sc -> AddObject ("PushGreenBins", pushGreenBins);
+	sc -> AddObject ("Auton2HardYellowTotes", ToteStack2Auton);
+	sc -> AddObject ("Auton3ToteStack", ToteStack3Auton);
 
 	SmartDashboard::PutData ("Auton Mode Chooser", sc);
 }
