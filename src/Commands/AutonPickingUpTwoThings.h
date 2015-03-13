203720f8bb04e1ddf507ae9fdac756fd9c435ae5
Sparky-1 <richardw@hesonline.com>
2015-03-12 20:35:21 -0400

    Autonomous Command. Lift bin, put on tote. Grab both. Back into scoring zone.

---
+++ src/Commands/AutonPickingUpTwoThings.h
@@ -0,0 +1,13 @@
+#ifndef AutonPickingUpTwoThings_H
+#define AutonPickingUpTwoThings_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class AutonPickingUpTwoThings: public CommandGroup
+{
+public:
+	AutonPickingUpTwoThings();
+};
+
+#endif
