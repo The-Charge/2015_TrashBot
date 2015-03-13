d693fd379789bfa9d3056a6104eb23a392a108ca
Sparky-1 <richardw@hesonline.com>
2015-03-13 17:59:28 -0400

    Added auton that moves back from landfill

---
+++ src/Commands/DriveFromLandfill.h
@@ -0,0 +1,13 @@
+#ifndef DriveFromLandfill_H
+#define DriveFromLandfill_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class DriveFromLandfill: public CommandGroup
+{
+public:
+	DriveFromLandfill();
+};
+
+#endif
