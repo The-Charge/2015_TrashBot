aca9df44dab7038e8943b824a955bf4465467b41
Sparky-1 <richardw@hesonline.com>
2015-04-06 19:17:21 -0400

    Added Command that runs the reeper in parallel to backing up. Added it to sendable chooser.

---
+++ src/Commands/AutonStrategy1ReeperParallel.h
@@ -0,0 +1,13 @@
+#ifndef AutonStrategy1ReeperParallel_H
+#define AutonStrategy1ReeperParallel_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class AutonStrategy1ReeperParallel: public CommandGroup
+{
+public:
+	AutonStrategy1ReeperParallel();
+};
+
+#endif
--- src/Commands/DriveXFeet.cpp
+++ src/Commands/DriveXFeet.cpp
@@ -26,7 +26,7 @@ DriveXFeet::DriveXFeet(float f, float m) {
 // Called just before this Command runs the first time
 void DriveXFeet::Initialize() {
 	this->SetTimeout( (1.0069 * feet)  - (6 * maxpercent) + 1.7448);
-
+	//8
 	Robot::driveTrain->ResetDistance();
 	Robot::driveTrain->SetDistanceInFeet(feet);
 	Robot::driveTrain->SetPIDControllerOutputRange(maxpercent);
