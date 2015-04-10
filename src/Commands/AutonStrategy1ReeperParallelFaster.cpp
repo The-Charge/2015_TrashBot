e65be552f4f612e3cc6031d3af9dacf345d3b05f
Sparky-1 <richardw@hesonline.com>
2015-04-10 14:03:20 -0400

    Created a faster autonreeperstrategy with shorter timeout on arm down. now have 3 total reeper autons.

---
+++ src/Commands/AutonStrategy1ReeperParallelFaster.cpp
@@ -0,0 +1,32 @@
+#include "AutonStrategy1ReeperParallelFaster.h"
+#include "DriveXFeetReeper.h"
+#include "BinRetrievalRaiseAuton.h"
+#include "BinRetrievalLowerAutonm.h"
+#include "DriveXFeet.h"
+
+AutonStrategy1ReeperParallelFaster::AutonStrategy1ReeperParallelFaster()
+{
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+	AddParallel (new DriveXFeetReeper(-.75, .25) );
+		AddSequential (new BinRetrievalRaiseAuton (2));
+		//AddSequential (new DelayCommand(1));
+		AddSequential (new DriveXFeet(8, .8));
+
+		AddParallel (new DriveXFeet(-1, .4));
+		AddSequential (new BinRetrievalLowerAutonm(4));
+}
---
+++ src/Commands/AutonStrategy1ReeperParallelFaster.h
@@ -0,0 +1,13 @@
+#ifndef AutonStrategy1ReeperParallelFaster_H
+#define AutonStrategy1ReeperParallelFaster_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class AutonStrategy1ReeperParallelFaster: public CommandGroup
+{
+public:
+	AutonStrategy1ReeperParallelFaster();
+};
+
+#endif
