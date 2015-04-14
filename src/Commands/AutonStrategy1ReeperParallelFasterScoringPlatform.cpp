1b0c4cf85a1645eed2f7f3ff14f11f0bf00d414f
Sparky-1 <richardw@hesonline.com>
2015-04-13 20:01:19 -0400

    Reeper changes from States

---
+++ src/Commands/AutonStrategy1ReeperParallelFasterScoringPlatform.cpp
@@ -0,0 +1,34 @@
+#include "AutonStrategy1ReeperParallelFasterScoringPlatform.h"
+#include "AutonStrategy1ReeperParallelFaster.h"
+#include "DriveXFeetReeper.h"
+#include "BinRetrievalRaiseAuton.h"
+#include "BinRetrievalLowerAutonm.h"
+#include "DriveXFeet.h"
+
+
+AutonStrategy1ReeperParallelFasterScoringPlatform::AutonStrategy1ReeperParallelFasterScoringPlatform()
+{
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+	AddParallel (new DriveXFeetReeper(-.75, .25) );
+		AddSequential (new BinRetrievalRaiseAuton (1));
+		//AddSequential (new DelayCommand(1));
+		AddSequential (new DriveXFeet(10, .8));
+
+		AddParallel (new DriveXFeet(-1, .4));
+		AddSequential (new BinRetrievalLowerAutonm(4));
+}
---
+++ src/Commands/AutonStrategy1ReeperParallelFasterScoringPlatform.h
@@ -0,0 +1,13 @@
+#ifndef AutonStrategy1ReeperParallelFasterScoringPlatform_H
+#define AutonStrategy1ReeperParallelFasterScoringPlatform_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class AutonStrategy1ReeperParallelFasterScoringPlatform: public CommandGroup
+{
+public:
+	AutonStrategy1ReeperParallelFasterScoringPlatform();
+};
+
+#endif
